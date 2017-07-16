e52f55135c8bcdcf350249d4ece19138eac43981
Federico Naum <fede.naum@gmail.com>
2017-07-16 20:17:50 +1000

    Change single with double quote to make expansion to work

--- vars/runPythonScript.groovy
+++ vars/runPythonScript.groovy
@@ -2,7 +2,7 @@ def call(String script = '') {
     if (!script){
        echo "To use this step you need to pass a valid python file from the resource section of ththis library"
     }
-    def runnableScript = libraryResource '$script'
+    def runnableScript = libraryResource "${script}"
     echo "running, ${script}."
     sh runnableScript
 }
