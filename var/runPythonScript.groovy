96bfddae2759c57d26e1655c0c6761ffde2e53de
Federico Naum <fede.naum@gmail.com>
2017-07-16 20:00:27 +1000

    Add runPythonScript step

---
+++ var/runPythonScript.groovy
@@ -0,0 +1,8 @@
+def call(String script = '') {
+    if (!script){
+       echo "To use this step you need to pass a valid python file from the resource section of ththis library"
+    }
+    def runnableScript = libraryResource '$script'
+    echo "running, ${script}."
+    sh runnableScript
+}
